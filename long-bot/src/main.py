c58814a91af0e31adac361a5852ba420cf215445
ekatsur435-dotcom <ekatsur435@gmail.com>
2026-04-13 00:46:39 +0300

    Update main.py

--- long-bot/src/main.py
+++ long-bot/src/main.py
@@ -59,7 +59,7 @@ from bot.telegram import TelegramBot, TelegramCommandHandler
 class Config:
     BOT_TYPE      = "long"
     MIN_SCORE     = int(os.getenv("MIN_LONG_SCORE", "65"))
-    SCAN_INTERVAL = int(os.getenv("SCAN_INTERVAL", "60"))
+    SCAN_INTERVAL = int(os.getenv("SCAN_INTERVAL", "120"))
     MAX_POSITIONS = int(os.getenv("MAX_LONG_POSITIONS", "5"))
     LEVERAGE      = os.getenv("LONG_LEVERAGE", "5-10")
     # SL НИЖЕ входа для LONG
